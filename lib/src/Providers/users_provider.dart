c6e1ebb7497753ea7163d3d7c02e1ad35a727ec1
gallottohugo <gallottohugo@gmail.com>
2020-07-14 12:26:48 -0300

    Refact users_provider

--- lib/src/Providers/users_provider.dart
+++ lib/src/Providers/users_provider.dart
@@ -1,31 +1,29 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_login_signup/src/preferences/preferences.dart';
+import 'package:flutter_login_signup/src/providers/server_provider.dart';
+import 'package:http/http.dart' as http;
 
 class UserProvider {
 	Future<Map<String, dynamic>> login ({@required String user, @required String password}) async{
-		
-		
-			//final url = '${ PaymunProvider.server }/api/auth/token/';
-			//final response = await http.post(url,  body: params);
-            //String body = utf8.decode(response.bodyBytes);
-			//final decodedData = json.decode(body);
+		try{
+			final url = '${ ServerProvider.server }/v1/auth/login';
+			Map<String, String> params = {
+				"email": "gallottohugo@gmail.com",
+				"password": "123456"
+			};
+			final response = await http.post(url,  body: params);
+            String body = utf8.decode(response.bodyBytes);
+			final decodedData = json.decode(body);
 
-			//if (response.statusCode == 200){
+			if (response.statusCode == 200){
 				//save token to preferences
-				//Preferences preferences = new Preferences();
-				//preferences.token       = decodedData['token'];
-				//preferences.userId      = decodedData['id'];
-				//preferences.name        = decodedData['name'];
-				//preferences.username    = decodedData['username'];
-				//preferences.phoneNumber = decodedData['phone_number'];
-				//return { 'ok': true };
-			//} else {
-				//final message = decodedData['non_field_errors'];
-				//return { 'ok': false, 'message': message[0]};
-			//}	
-		//} catch (exception){ 
-			//return { 'ok': false, 'message': exception.toString() }; 
-		//}
-		return { "ok": true };
+				Preferences preferences = new Preferences();
+				preferences.token       = decodedData['token'];
+				preferences.userId      = decodedData['username'];
+				return { 'ok': true };
+			} else { return { 'ok': false }; }	
+		} catch (exception){ return { 'ok': false }; }
 	}
 }
 
