aabd6a490a229c444d078c7e29587b02c77c6da6
gallottohugo <gallottohugo@gmail.com>
2020-07-15 17:48:41 -0300

    Add properties to preferences

--- lib/src/preferences/preferences.dart
+++ lib/src/preferences/preferences.dart
@@ -16,4 +16,37 @@ class Preferences {
 	get userId {return _prefs.getInt('userId') ?? 1;}
 	set userId(int value){_prefs.setInt('userId', value);}
 
+  //settings admin
+	get userAdmin {return _prefs.getBool('userAdmin') ?? false;}
+	set userAdmin(bool value){_prefs.setBool('userAdmin', value);}
+
+  //settings userName
+	get userName { return _prefs.getString('userName') ?? ''; }
+	set userName(String value){ _prefs.setString('userName', value);}
+
+  //settings userLastName
+	get userLastName { return _prefs.getString('userLastName') ?? ''; }
+	set userLastName(String value){ _prefs.setString('userLastName', value);}
+
+  //settings userEmail
+	get userEmail { return _prefs.getString('userEmail') ?? ''; }
+	set userEmail(String value){ _prefs.setString('userEmail', value);}
+
+  //settings userType
+	get userType { return _prefs.getString('userType') ?? ''; }
+	set userType(String value){ _prefs.setString('userType', value);}
+  
+
+
+  	static void clearPreferences(){
+    	Preferences preferences = Preferences();
+		preferences.token = '';
+		preferences.userId = 0;
+		preferences.userAdmin = false;
+		preferences.userName = '';
+		preferences.userLastName = '';
+		preferences.userEmail = '';
+		preferences.userType = '';
+  	}
+
 }
