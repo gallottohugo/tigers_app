d91b115f1c43436555801e3adba40cf7da15c4d0
gallottohugo <gallottohugo@gmail.com>
2020-07-16 15:16:56 -0300

    add pages to routes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_login_signup/src/pages/districts/districts_create_page.dart';
+import 'package:flutter_login_signup/src/pages/districts/districts_list_page.dart';
 import 'package:flutter_login_signup/src/pages/home/home_page.dart';
-import 'package:flutter_login_signup/src/pages/users/users_customers_list.dart';
+import 'package:flutter_login_signup/src/pages/houses/houses_create_page.dart';
+import 'package:flutter_login_signup/src/pages/users/users_customers_list_page.dart';
 import 'package:flutter_login_signup/src/pages/users/users_houses_page.dart';
 import 'package:flutter_login_signup/src/pages/users/users_list_page.dart';
 import 'package:flutter_login_signup/src/pages/users/users_login_page.dart';
@@ -10,12 +13,23 @@ class ApplicationRoutes {
 	
 	static Map<String, WidgetBuilder> getApplicationRoutes(){
 		return <String, WidgetBuilder> {
+			//home
 			HomePage.routeName   : (BuildContext context)  => HomePage(),
+
+			//users
 			UsersLoginPage.routeName  : (BuildContext context)  => UsersLoginPage(),
 			UsersCreatePage.routeName : (BuildContext context)  => UsersCreatePage(),
       		UsersHousesPage.routeName : (BuildContext context) => UsersHousesPage(),
 			UsersCustomersListPage.routeName : (BuildContext context) => UsersCustomersListPage(),
 			UsersListPage.routeName : (BuildContext context) => UsersListPage(),
+			
+			//houses
+			HousesCreatePage.routeName : (BuildContext context) => HousesCreatePage(),
+
+			//districts
+			DistrictsCreatePage.routeName : (BuildContext context) => DistrictsCreatePage(),
+			DistrictsListPage.routeName : (BuildContext context) => DistrictsListPage(),
+			
 		};
 	}
 }
