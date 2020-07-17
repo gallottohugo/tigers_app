e2bb6b623bfb6d0f0173b32c038a46385b5b9627
gallottohugo <gallottohugo@gmail.com>
2020-07-17 17:41:24 -0300

    Refact

--- lib/src/Pages/districts/districts_create_page.dart
+++ lib/src/Pages/districts/districts_create_page.dart
@@ -5,6 +5,9 @@ import 'package:flutter_login_signup/src/models/district_model.dart';
 import 'package:flutter_login_signup/src/providers/districts_provider.dart';
 import 'package:flutter_login_signup/src/widgets/alert_widgets.dart';
 import 'package:flutter_login_signup/src/widgets/bezierContainer.dart';
+import 'package:flutter_login_signup/src/widgets/button_widget.dart';
+import 'package:flutter_login_signup/src/widgets/progress_indicator_widget.dart';
+import 'package:flutter_login_signup/src/widgets/text_form_field_widget.dart';
 
 class DistrictsCreatePage extends StatefulWidget {
 	static final String routeName = 'district_create_page';
@@ -27,11 +30,7 @@ class _DistrictsCreatePageState extends State<DistrictsCreatePage> {
         		height: height,
         		child: Stack(
           			children: <Widget>[
-            			Positioned(
-              				top: -MediaQuery.of(context).size.height * .15,
-              				right: -MediaQuery.of(context).size.width * .4,
-              				child: BezierContainer(),
-            			),
+            			BezierContainer(),
             			Container(
               				padding: EdgeInsets.symmetric(horizontal: 20),
               				child: SingleChildScrollView(
@@ -41,27 +40,11 @@ class _DistrictsCreatePageState extends State<DistrictsCreatePage> {
                   					children: <Widget>[
                     					SizedBox(height: 20,),
                     					_formWidget(),
-                    					SizedBox(height: 20,),
-                    					_submitButton(),
-                    					SizedBox(height: height * .14),
                   					],
                 				),
               				),
             			),
-						showLoading == true ? AbsorbPointer(
-							child: BackdropFilter(
-								filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-								child: Container(
-									padding: EdgeInsets.only(top: 20),
-									child: Center(
-											child: CircularProgressIndicator(
-											backgroundColor: Colors.transparent,
-											valueColor: new AlwaysStoppedAnimation<Color>(Color(0xffe46b10)),
-										)
-									)
-								)
-							)
-						) : Container()
+						showLoading == true ? ProgressIndicatorWidget() : Container()
           			],
         		),
       		),
@@ -75,72 +58,22 @@ class _DistrictsCreatePageState extends State<DistrictsCreatePage> {
 			preferredSize: Size.fromHeight(60.0), // here the desired height
 			child: _appBarTiger(leading: leading,)
 		);
-	}
-
-	  
-  
-
-  	Widget _entryField(String title, {@required  String initialValue, @required bool enabled, Function onSaved, @required TextInputType textInputType  }) {
-    	return Container(
-      		margin: EdgeInsets.symmetric(vertical: 10),
-      		child: Column(
-        		crossAxisAlignment: CrossAxisAlignment.start,
-        		children: <Widget>[
-					Text(title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-					SizedBox(height: 10,),
-					TextFormField(
-						decoration: InputDecoration(
-							border: InputBorder.none,
-							fillColor: Color(0xfff3f3f4),
-							filled: true
-						),
-						keyboardType: textInputType,
-						onSaved: onSaved,
-						initialValue: initialValue,
-						enabled: enabled,
-					)
-        		],
-      		),
-    	);
-  	}
-
-  	Widget _submitButton() {
-    	return GestureDetector(
-			child: Container(
-				width: MediaQuery.of(context).size.width,
-				padding: EdgeInsets.symmetric(vertical: 15),
-				alignment: Alignment.center,
-				decoration: BoxDecoration(
-					borderRadius: BorderRadius.all(Radius.circular(5)),
-					boxShadow: <BoxShadow>[BoxShadow(color: Colors.grey.shade200,offset: Offset(2, 4),blurRadius: 5,spreadRadius: 2)],
-					gradient: LinearGradient(
-						begin: Alignment.centerLeft,
-						end: Alignment.centerRight,
-						colors: [Color(0xfffbb448), Color(0xfff7892b)]
-					)
-				),
-				child: Text('Crear', style: TextStyle(fontSize: 20, color: Colors.white)),
-			),
-			onTap: () async {
-				setState(() { showLoading = true; });
-				formKey.currentState.save();
-				DistrictsProvider districtsProvider = DistrictsProvider();
-				Map<String, dynamic> response = await districtsProvider.districtsCreate(district: newDistrict);
-				setState(() { showLoading = false; });
-				if (response["ok"] == true) {
-					Navigator.pop(context);
-				} else {
-					AlertWidgets.alertOkWidget(context, 'Error', response["message"], Icon(Icons.error));
-				}
-				
-			},
-    	);
-	}
-
-	
+	}	
 
 
 	void _onSavedName(String value){ newDistrict.name = value; }
+	void _onTapButton() async {
+		setState(() { showLoading = true; });
+		formKey.currentState.save();
+		DistrictsProvider districtsProvider = DistrictsProvider();
+		Map<String, dynamic> response = await districtsProvider.districtsCreate(district: newDistrict);
+		setState(() { showLoading = false; });
+		if (response["ok"] == true) {
+			Navigator.pop(context);
+		} else {
+			AlertWidgets.alertOkWidget(context, 'Error', response["message"], Icon(Icons.error));
+		}
+	}
 	
 
   	Widget _formWidget() {
@@ -148,7 +81,9 @@ class _DistrictsCreatePageState extends State<DistrictsCreatePage> {
 			key: formKey,
 			child: Column(
 				children: <Widget>[
-					_entryField("Nombre", onSaved: _onSavedName, enabled: true, initialValue: '', textInputType: TextInputType.text),
+					TextFormFieldWidget(title: 'Nombre', enabled: true, initialValue: '', obscureText: false, onSaved: _onSavedName, textInputType: TextInputType.text,),
+					SizedBox(height: 20,),
+					ButtonWidget(title: 'Crear', border: Colors.white, colorStart: Color(0xfffbb448), colorEnd: Color(0xfff7892b), colorText: Colors.black, onTapFunction: _onTapButton,)
 				],
 			)
     	);
