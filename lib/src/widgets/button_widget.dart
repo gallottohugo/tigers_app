cb27799f3bb17b7cf4eb8ff67bd2b83fbf5f41fb
gallottohugo <gallottohugo@gmail.com>
2020-07-23 11:47:01 -0300

    Refact button widget

--- lib/src/widgets/button_widget.dart
+++ lib/src/widgets/button_widget.dart
@@ -2,15 +2,25 @@ import 'package:flutter/material.dart';
 
 class ButtonWidget extends StatelessWidget {
 	final String title;
-	final Function onTapFunction;
+	final Function onPressedFunction;
 	final Color colorStart;
 	final Color colorEnd;
 	final Color border;
 	final Color colorText;
-  	const ButtonWidget({Key key, this.title, this.onTapFunction, this.border, this.colorEnd, this.colorStart, this.colorText}) : super(key: key);
+	final Stream stream;
+  	const ButtonWidget({
+		Key key, 
+		this.title, 
+		this.onPressedFunction, 
+		this.border, 
+		this.colorEnd, 
+		this.colorStart, 
+		this.colorText,
+		this.stream}) : super(key: key);
 
   	@override
   	Widget build(BuildContext context) {	
+		/*
     	return GestureDetector(
 			child: Container(
 				width: MediaQuery.of(context).size.width,
@@ -27,6 +37,34 @@ class ButtonWidget extends StatelessWidget {
 				child: Text(this.title, style: TextStyle(fontSize: 20, color: this.colorText)),
 			),
 			onTap: this.onTapFunction
-    	);
+    	);*/
+
+		//formLoginStream
+
+		return StreamBuilder(
+		  	stream: stream,
+		  	builder: (BuildContext context, AsyncSnapshot snapshot){
+				return RaisedButton(
+					elevation: 0,
+					color: Colors.transparent,
+					disabledColor: Colors.transparent,
+					child: Container(
+						width: MediaQuery.of(context).size.width,
+						padding: EdgeInsets.symmetric(vertical: 15),
+						alignment: Alignment.center,
+						decoration: BoxDecoration(
+							borderRadius: BorderRadius.all(Radius.circular(5)),
+							gradient: LinearGradient(
+								begin: Alignment.centerLeft,
+								end: Alignment.centerRight,
+								colors: snapshot.hasData ? [colorStart, colorEnd] : [Colors.grey, Colors.grey]
+							)
+						),
+						child: Text(this.title, style: TextStyle(fontSize: 20, color: this.colorText)),
+					),
+					onPressed: snapshot.hasData ? this.onPressedFunction : null,
+				);
+		  	},
+		);		
 	}
 }
