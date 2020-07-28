c616c7ca5cd682c7cafea1bc256c5ecc2b16ece5
gallottohugo <gallottohugo@gmail.com>
2020-07-28 09:31:41 -0300

    Create bloc widgets

---
+++ lib/src/widgets/button_bloc_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ButtonBlocWidget extends StatelessWidget {
+	final String title;
+	final Function onPressedFunction;
+	final Color colorStart;
+	final Color colorEnd;
+	final Color border;
+	final Color colorText;
+	final Stream stream;
+  	const ButtonBlocWidget({
+		Key key, 
+		this.title, 
+		this.onPressedFunction, 
+		this.border, 
+		this.colorEnd, 
+		this.colorStart, 
+		this.colorText,
+		this.stream}) : super(key: key);
+
+  	@override
+  	Widget build(BuildContext context) {	
+		return StreamBuilder(
+		  	stream: stream,
+		  	builder: (BuildContext context, AsyncSnapshot snapshot){
+				return RaisedButton(
+					elevation: 0,
+					color: Colors.transparent,
+					disabledColor: Colors.transparent,
+					child: Container(
+						width: MediaQuery.of(context).size.width,
+						padding: EdgeInsets.symmetric(vertical: 15),
+						alignment: Alignment.center,
+						decoration: BoxDecoration(
+							borderRadius: BorderRadius.all(Radius.circular(5)),
+							gradient: LinearGradient(
+								begin: Alignment.centerLeft,
+								end: Alignment.centerRight,
+								colors: snapshot.hasData ? [colorStart, colorEnd] : [Colors.grey, Colors.grey]
+							)
+						),
+						child: Text(this.title, style: TextStyle(fontSize: 20, color: this.colorText)),
+					),
+					onPressed: snapshot.hasData ? this.onPressedFunction : null,
+				);
+		  	},
+		);		
+	}
+}
---
+++ lib/src/widgets/text_form_field_bloc_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+
+class TextFormFieldBlocWidget extends StatefulWidget {
+	final String title;
+	final String initialValue;
+	final bool enabled;
+	final Function onSavedFunction;
+  	final Function onTapFunction;
+	final Function onChangedFunction;
+	final TextInputType textInputType;
+	final bool obscureText;
+	final TextEditingController controller;
+	final Stream stream;
+
+  	TextFormFieldBlocWidget({
+		Key key, 
+		this.enabled, 
+		this.initialValue, 
+		this.onSavedFunction, 
+		this.textInputType, 
+		this.title, 
+		this.obscureText = false, 
+		this.onTapFunction, 
+		this.onChangedFunction, 
+		this.controller,
+		this.stream}) : super(key: key);
+  	_TextFormFieldBlocWidgetState createState() => _TextFormFieldBlocWidgetState();
+}
+
+
+class _TextFormFieldBlocWidgetState extends State<TextFormFieldBlocWidget> {
+  	@override
+  	Widget build(BuildContext context) {
+		return StreamBuilder(
+		  	stream: widget.stream ,
+		  	builder: (BuildContext context, AsyncSnapshot snapshot){
+				  return Container(
+					margin: EdgeInsets.symmetric(vertical: 10),
+					child: Column(
+						crossAxisAlignment: CrossAxisAlignment.start,
+						children: <Widget>[
+							Text(widget.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+							SizedBox(height: 10,),
+							TextFormField(
+								decoration: InputDecoration(
+									border: InputBorder.none,
+									fillColor: Color(0xfff3f3f4),
+									filled: true,
+									//counterText: snapshot.data
+									errorText: snapshot.error
+								),
+								keyboardType: widget.textInputType,
+								onSaved: widget.onSavedFunction,
+								initialValue: widget.initialValue,
+								enabled: widget.enabled,
+								obscureText: widget.obscureText,
+								onTap: widget.onTapFunction,
+								onChanged: widget.onChangedFunction,
+								controller: widget.controller,
+							)
+						],
+					),
+				);
+		  	},
+		);
+  	}
+}
