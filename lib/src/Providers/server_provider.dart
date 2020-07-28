c6637fd2c796a07029c89c92bc2f3ad0fb5877d7
gallottohugo <gallottohugo@gmail.com>
2020-07-28 09:32:57 -0300

    Add production server

--- lib/src/Providers/server_provider.dart
+++ lib/src/Providers/server_provider.dart
@@ -1,3 +1,4 @@
 class ServerProvider{
-  	static String server = "http://3.128.219.77:3000";
+  	//static String server = "http://3.128.219.77:3000";
+	static String server = "http://3.128.219.77";
 }
