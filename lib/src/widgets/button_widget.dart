d21ed3f13f5d3ba6b93816d1025fec16045f7c5f
gallottohugo <gallottohugo@gmail.com>
2020-07-28 09:34:35 -0300

    Refact buttonWidget

--- lib/src/widgets/button_widget.dart
+++ lib/src/widgets/button_widget.dart
@@ -7,7 +7,6 @@ class ButtonWidget extends StatelessWidget {
 	final Color colorEnd;
 	final Color border;
 	final Color colorText;
-	final Stream stream;
   	const ButtonWidget({
 		Key key, 
 		this.title, 
@@ -15,13 +14,14 @@ class ButtonWidget extends StatelessWidget {
 		this.border, 
 		this.colorEnd, 
 		this.colorStart, 
-		this.colorText,
-		this.stream}) : super(key: key);
+		this.colorText,}) : super(key: key);
 
   	@override
   	Widget build(BuildContext context) {	
-		/*
-    	return GestureDetector(
+		return RaisedButton(
+			elevation: 0,
+			color: Colors.transparent,
+			disabledColor: Colors.transparent,
 			child: Container(
 				width: MediaQuery.of(context).size.width,
 				padding: EdgeInsets.symmetric(vertical: 15),
@@ -36,35 +36,7 @@ class ButtonWidget extends StatelessWidget {
 				),
 				child: Text(this.title, style: TextStyle(fontSize: 20, color: this.colorText)),
 			),
-			onTap: this.onTapFunction
-    	);*/
-
-		//formLoginStream
-
-		return StreamBuilder(
-		  	stream: stream,
-		  	builder: (BuildContext context, AsyncSnapshot snapshot){
-				return RaisedButton(
-					elevation: 0,
-					color: Colors.transparent,
-					disabledColor: Colors.transparent,
-					child: Container(
-						width: MediaQuery.of(context).size.width,
-						padding: EdgeInsets.symmetric(vertical: 15),
-						alignment: Alignment.center,
-						decoration: BoxDecoration(
-							borderRadius: BorderRadius.all(Radius.circular(5)),
-							gradient: LinearGradient(
-								begin: Alignment.centerLeft,
-								end: Alignment.centerRight,
-								colors: snapshot.hasData ? [colorStart, colorEnd] : [Colors.grey, Colors.grey]
-							)
-						),
-						child: Text(this.title, style: TextStyle(fontSize: 20, color: this.colorText)),
-					),
-					onPressed: snapshot.hasData ? this.onPressedFunction : null,
-				);
-		  	},
-		);		
+			onPressed:  this.onPressedFunction
+		);
 	}
 }
