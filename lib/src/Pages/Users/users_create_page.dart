c609e7548b5bb2ddeb7168b8db9adb6c8ca12729
gallottohugo <gallottohugo@gmail.com>
2020-07-29 13:31:48 -0300

    Refact

--- lib/src/Pages/Users/users_create_page.dart
+++ lib/src/Pages/Users/users_create_page.dart
@@ -8,7 +8,6 @@ import 'package:flutter_login_signup/src/widgets/app_bar_widget.dart';
 import 'package:flutter_login_signup/src/widgets/bezierContainer.dart';
 import 'package:flutter_login_signup/src/widgets/button_widget.dart';
 import 'package:flutter_login_signup/src/widgets/progress_indicator_page_widget.dart';
-import 'package:flutter_login_signup/src/widgets/progress_indicator_widget.dart';
 import 'package:flutter_login_signup/src/widgets/text_form_field_widget.dart';
 
 class UsersCreatePage extends StatefulWidget {
